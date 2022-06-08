90b1e6a6813d01776449018128134cc9449fadc8
ptlong1 <longlcqt@gmail.com>
2022-06-08 20:11:15 +0700

    -new build fix slide

--- static/Multiplayer-WebGL/Build/Multiplayer-WebGL.framework.js
+++ static/Multiplayer-WebGL/Build/Multiplayer-WebGL.framework.js
@@ -1312,7 +1312,7 @@ function _emscripten_asm_const_ii(code, a0) {
  return ASM_CONSTS[code](a0);
 }
 STATIC_BASE = GLOBAL_BASE;
-STATICTOP = STATIC_BASE + 4899280;
+STATICTOP = STATIC_BASE + 4899744;
 __ATINIT__.push({
  func: (function() {
   __GLOBAL__sub_I_AccessibilityScriptingClasses_cpp();
@@ -3366,7 +3366,7 @@ __ATINIT__.push({
   ___emscripten_environ_constructor();
  })
 });
-var STATIC_BUMP = 4899280;
+var STATIC_BUMP = 4899744;
 Module["STATIC_BASE"] = STATIC_BASE;
 Module["STATIC_BUMP"] = STATIC_BUMP;
 var tempDoublePtr = STATICTOP;
